2c9fdf5ad93955033fc106a51476062dc95817b7
Philipp Piwo <github@piwo.space>
2019-08-15 10:03:13 +0200

    preparation for deployment

---
+++ governance-aepp/deploy.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+GIT_REV="$(git rev-parse HEAD)"&& \
+rm -rf dist && \
+rm -rf node_modules && \
+npm install && \
+NODE_ENV=prod npm run build && \
+cd dist/ && \
+git init && \
+git remote add origin git@github.com:aeternity/aepp-governance.git && \
+git checkout -b gh-pages && \
+git add * && \
+git commit -m "governance aepp ${GIT_REV} deployment to gh-pages" && \
+git fetch && git rebase -s recursive -Xtheirs origin/gh-pages && \
+git push origin gh-pages
---
+++ governance-server/deploy.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+cd ../server
+docker build -t aeternity/governance-server .
+docker push aeternity/governance-server:latest
+
+ssh governance-server 'docker-compose pull; docker-compose up -d'
+ssh governance-server 'docker image prune -af'
