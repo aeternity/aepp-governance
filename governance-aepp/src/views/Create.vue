b205d08ddf699a0c435204860938b963f50f007b
keno dressel <aeternity@keno.digital>
2019-09-04 15:21:19 +0200

    adjusted formatting for create

--- governance-aepp/src/views/Create.vue
+++ governance-aepp/src/views/Create.vue
@@ -26,7 +26,7 @@
       </ul>
     </div>
     <div class="py-2 px-4">
-      <ae-input :error="!!errors.titleError"  v-model="createMetadata.title" label="Title">
+      <ae-input :error="!!errors.titleError" v-model="createMetadata.title" label="Title">
       </ae-input>
     </div>
     <div class="py-2 px-4">
@@ -46,20 +46,23 @@
       </AeButtonGroup>
     </div>
 
-    <div class="my-2 mx-4 flex items-center bg-white px-4" :class="{'bg-ae-error': !!errors.optionError}" v-for="option in options">
+    <div class="my-2 mx-4 flex items-center bg-white px-4" :class="{'bg-ae-error': !!errors.optionError}"
+         v-for="option in options">
       <div class="w-6 flex justify-center">
         <div v-if="option.text" class="rounded-full border-2 border-gray-500 w-6 h-6">&nbsp;</div>
         <div v-else class="text-3xl text-gray-300 text-right font-bold">&plus;</div>
       </div>
       <input v-model="option.text" @input="optionInput" type="text" placeholder="Add Option"
-             class="ae-input-option w-full h-full px-2 py-6 outline-none" :class="{'bg-ae-error': !!errors.optionError}" />
+             class="ae-input-option w-full h-full px-2 py-6 outline-none"
+             :class="{'bg-ae-error': !!errors.optionError}"/>
       <div v-if="option.text">
-        <div class="text-2xl text-gray-500 text-right"  @click="removeOption(option.id)">&times;</div>
+        <div class="text-2xl text-gray-500 text-right" @click="removeOption(option.id)">&times;</div>
       </div>
     </div>
 
     <div class="py-2 px-4 mb-16">
-      <ae-input :error="!!errors.closeHeightError" type="number" v-model="closeHeightString" label="Close at height (optional)"></ae-input>
+      <ae-input :error="!!errors.closeHeightError" type="number" v-model="closeHeightString"
+                label="Close at height (optional)"></ae-input>
       <div class="text-gray-500 text-sm p-2">
         Current height is {{height}}
         <span v-if="closeDate">
@@ -125,33 +128,33 @@
     methods: {
       async createPoll() {
 
-        this.errors =  {
+        this.errors = {
           titleError: null,
-            descriptionError: null,
-            linkError: null,
-            optionError: null,
-            closeHeightError: null
-        }
+          descriptionError: null,
+          linkError: null,
+          optionError: null,
+          closeHeightError: null
+        };
 
         // VERIFY INPUT
         this.createMetadata.title = this.createMetadata.title.trim();
-        if(this.createMetadata.title.length === 0) this.errors.titleError = 'Please provide a title.';
-        if(this.createMetadata.title.length > 50) this.errors.titleError = 'Your title is too long (50 chars max).';
+        if (this.createMetadata.title.length === 0) this.errors.titleError = 'Please provide a title.';
+        if (this.createMetadata.title.length > 50) this.errors.titleError = 'Your title is too long (50 chars max).';
 
         this.createMetadata.description = this.createMetadata.description.trim();
-        if(this.createMetadata.description.length === 0) this.errors.descriptionError = 'Please provide a description.';
+        if (this.createMetadata.description.length === 0) this.errors.descriptionError = 'Please provide a description.';
 
         this.createMetadata.link = this.createMetadata.link.trim();
-        if(this.createMetadata.link.indexOf('http') === -1) this.errors.linkError = 'Your link must include http:// or https://.';
-        if(this.createMetadata.link.length === 0) this.errors.linkError = 'Please provide a link.';
+        if (this.createMetadata.link.indexOf('http') === -1) this.errors.linkError = 'Your link must include http:// or https://.';
+        if (this.createMetadata.link.length === 0) this.errors.linkError = 'Please provide a link.';
 
         let options = this.options.filter(option => !!(option.text.trim()));
-        if(options.length < 2) this.errors.optionError = 'Please provide at least two options.';
+        if (options.length < 2) this.errors.optionError = 'Please provide at least two options.';
 
-        if(this.closeHeightString.length > 0 && isNaN(parseInt(this.closeHeightString))) this.errors.closeHeightError = 'The closing height is not a whole number.';
-        else if(this.closeHeightString.length > 0 && parseInt(this.closeHeightString) <= this.height) this.errors.closeHeightError = 'The closing height lies in the past.';
+        if (this.closeHeightString.length > 0 && isNaN(parseInt(this.closeHeightString))) this.errors.closeHeightError = 'The closing height is not a whole number.';
+        else if (this.closeHeightString.length > 0 && parseInt(this.closeHeightString) <= this.height) this.errors.closeHeightError = 'The closing height lies in the past.';
 
-        if(Object.values(this.errors).some(val => !!val)) return document.querySelector('.wrapper').scrollTo(0,0);
+        if (Object.values(this.errors).some(val => !!val)) return document.querySelector('.wrapper').scrollTo(0, 0);
 
         // SUBMIT
 
@@ -164,7 +167,7 @@
             .map(option => {
               option.id = newID++;
               return option
-            }).reduce((acc, option)=> Object.assign(acc, {[option.id]: option.text}), {});
+            }).reduce((acc, option) => Object.assign(acc, {[option.id]: option.text}), {});
 
           const pollContract = await aeternity.client.getContractInstance(pollContractSource);
           const init = await pollContract.methods.init(this.createMetadata, options, close_height);
@@ -201,6 +204,7 @@
     line-height: 1.5rem;
     font-family: Inter UI, sans-serif;
   }
+
   .bg-ae-error {
     background-color: #ffeeee;
     /* color: #ff0d0d; */
