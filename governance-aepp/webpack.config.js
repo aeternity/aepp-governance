be627e79804240f7157288f9f574864f888166cc
Keno Dreßel <hire@keno.digital>
2019-09-17 12:12:40 +0200

    updates manifest generation to include favicon (fixes #70)

--- governance-aepp/webpack.config.js
+++ governance-aepp/webpack.config.js
@@ -6,8 +6,8 @@ const HtmlWebpackHarddiskPlugin = require('html-webpack-harddisk-plugin')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const { VueLoaderPlugin } = require('vue-loader')
 // const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
-const PurgecssPlugin = require('purgecss-webpack-plugin')
-const WebpackPwaManifest = require('webpack-pwa-manifest');
+const PurgecssPlugin = require('purgecss-webpack-plugin');
+const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
 let glob = require('glob-all')
 
 const distFolder = path.resolve(__dirname, 'dist')
@@ -77,17 +77,22 @@ module.exports = env => {
       new HtmlWebpackHarddiskPlugin(),
       new CleanWebpackPlugin(),
       new VueLoaderPlugin(),
-      new WebpackPwaManifest({
-        name: 'Governance',
-        short_name: 'Governance',
-        description: 'Create and vote on governance polls for aeternity.',
-        background_color: '#ff0d6a',
-        icons: [
-          {
-            src: path.resolve(__dirname, 'src/assets/favicon.svg'),
-            sizes: [96, 128, 192, 256, 384, 512] // multiple sizes
+      new FaviconsWebpackPlugin({
+        logo: path.resolve(__dirname, 'src/assets/favicon.svg'),
+        mode: 'webapp', // optional can be 'webapp' or 'light' - 'webapp' by default
+        devMode: 'webapp', // optional can be 'webapp' or 'light' - 'light' by default
+        favicons: {
+          appName: 'Governance',
+          description: 'Create and vote on governance polls for aeternity.',
+          developerName: 'Aeternity Developers',
+          developerURL: 'https://github.com/aeternity/aepp-governance',
+          background: '#ff0d6a',
+          theme_color: '#ff0d6a',
+          icons: {
+            coast: false,
+            yandex: false
           }
-        ]
+        }
       })
     ],
     module: {
