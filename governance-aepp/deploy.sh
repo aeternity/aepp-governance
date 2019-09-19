172c924b425bbc9c85ae36e321b635117126874c
Keno Dreßel <hire@keno.digital>
2019-09-19 16:27:51 +0200

    updates deploy script to discard old files

--- governance-aepp/deploy.sh
+++ governance-aepp/deploy.sh
@@ -4,12 +4,18 @@ GIT_REV="$(git rev-parse HEAD)"&& \
 rm -rf dist && \
 rm -rf node_modules && \
 npm install && \
-NODE_ENV=prod npm run build && \
-cd dist/ && \
+npm run build && \
+mkdir deployment && \
+cd deployment/ && \
 git init && \
 git remote add origin git@github.com:aeternity/aepp-governance.git && \
-git checkout -b gh-pages && \
+git fetch && \
+git checkout gh-pages && \
+git rm -rf . && \
+git clean -ffxd && \
+cp -r ../dist/* . && \
 git add * && \
 git commit -m "governance aepp ${GIT_REV} deployment to gh-pages" && \
 git fetch && git rebase -s recursive -Xtheirs origin/gh-pages && \
-git push origin gh-pages
+git push origin gh-pages && \
+rm -rf deployment
