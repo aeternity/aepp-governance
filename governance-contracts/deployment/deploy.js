f937b6607de01f4a583bd5e41e2126c914fcc46a
Philipp Piwo <github@piwo.space>
2019-11-15 12:15:42 +0100

    fix contract deployment script for testnet/mainnet

--- governance-contracts/deployment/deploy.js
+++ governance-contracts/deployment/deploy.js
@@ -20,7 +20,7 @@ const Universal = require('@aeternity/aepp-sdk').Universal;
 const deploy = async (network, privateKey, compiler) => {
     let deployer = new Deployer(network, privateKey, compiler);
 
-    const client = await Universal({
+    /*const client = await Universal({
         url: deployer.network.url,
         internalUrl: deployer.network.url,
         keypair: deployer.keypair,
@@ -28,12 +28,12 @@ const deploy = async (network, privateKey, compiler) => {
         compilerUrl: deployer.compilerUrl
     });
 
-    
+
     await client.spend(100000000000000000000, "ak_y87WkN4C4QevzjTuEYHg6XLqiWx3rjfYDFLBmZiqiro5mkRag");
     await client.spend(50000000000000000000, "ak_9jFvDQKvq6Viap476momQDjbPxf17pDgKuMv6dseryxdpaZUF");
     await client.spend(20000000000000000000, "ak_2wV4puWGcrV8rLxrRaPrFuMrgkUG413UvmgNeVdyqxKmUMSkEk");
     await client.spend(10000000000000000000, "ak_xFowwp6E9t2mbHpjb2a8dGCMHSr7f3LxHAtW65qxrti3nDKKk");
-    await client.spend(1000000000000000000, "ak_11111111111111111111111111111111273Yts");
+    await client.spend(1000000000000000000, "ak_11111111111111111111111111111111273Yts");*/
 
     await deployer.deploy("./contracts/Registry.aes")
 };
