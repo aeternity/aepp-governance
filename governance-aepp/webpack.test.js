10d35dbe5fbf8a8c2ed50c92b0bd8647d8189de4
keno dressel <aeternity@keno.digital>
2019-12-19 13:38:27 +0100

    minimizes webpack test config

--- governance-aepp/webpack.test.js
+++ governance-aepp/webpack.test.js
@@ -1,53 +1,8 @@
 const merge = require('webpack-merge');
-const common = require('./webpack.common.js');
+const production = require('./webpack.prod.js');
 const webpack = require('webpack');
 
-const path = require('path');
-
-
-module.exports = merge(common, {
-  mode: 'production',
-  output: {
-    filename: 'bundle.js?[hash]',
-    publicPath: './'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: [
-          'vue-style-loader',
-          'css-loader',
-          {
-            loader: 'postcss-loader',
-            options: {
-              ident: 'postcss',
-              plugins: [
-                require('postcss-import'),
-                require('tailwindcss'),
-                require('autoprefixer'),
-                require('@fullhuman/postcss-purgecss')({
-                  content: [
-                    path.join(__dirname, './src/index.html'),
-                    path.join(__dirname, './**/*.vue'),
-                    path.join(__dirname, './src/**/*.js')
-                  ],
-                  whitelistPatterns: [/^ae/],
-                  defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
-                }),
-                require('cssnano')({
-                  'preset': [
-                    'default',
-                    {'discardComments': {'removeAll': true}}
-                  ]
-                })
-              ]
-            }
-          }
-        ]
-      }
-    ]
-  },
+module.exports = merge(production,  {
   plugins: [
     new webpack.EnvironmentPlugin(['PRIVATE_KEY', 'PUBLIC_KEY'])
   ]
