aa40b13e73639e13e97f0ca9236396719e5fce00
Philipp Piwo <github@piwo.space>
2022-10-04 13:31:49 +0200

    app, compare verified hashes for verification

--- governance-aepp/vue.config.js
+++ governance-aepp/vue.config.js
@@ -1,9 +1,13 @@
+const NodePolyfillPlugin = require("node-polyfill-webpack-plugin");
+
 module.exports = {
   transpileDependencies: [
     '@aeternity'
   ],
   // added file-loader
   chainWebpack: config => {
+    config.plugin('polyfills').use(NodePolyfillPlugin)
+
     // .aes loader
     config.module
       .rule('aes')
